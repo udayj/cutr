37962259470eae0732d05dc5db0e9853d36c8700
root <jhunjhunwala.uday@gmail.com>
2023-03-16 16:24:21 +0000

    parse field ranges

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use crate::Extract::*;
 use clap::{App, Arg};
-use std::{error::Error, ops::Range};
+use std::{error::Error, ops::Range, num::NonZeroUsize};
+use regex::Regex;
 
 type MyResult<T> = Result<T, Box< dyn Error>>;
 type PositionList = Vec<Range<usize>>;
@@ -79,7 +80,7 @@ pub fn get_args() -> MyResult<Config> {
                 
 }
 
-fn parse_pos(range: &str) -> MyResult<PositionList> {
+fn parse_pos_basic(range: &str) -> MyResult<PositionList> {
 
     let mut result:PositionList = PositionList::new();
 
@@ -96,4 +97,50 @@ fn parse_pos(range: &str) -> MyResult<PositionList> {
         
     }
     return Ok(result);
+}
+
+fn parse_index(input: &str) -> Result<usize, String> {
+
+    let value_error = || format!(
+        "illegal list value: \"{}\"", input);
+
+    input.starts_with('+')
+        .then(|| Err(value_error()))
+        .unwrap_or_else(
+            || {
+                input.parse::<NonZeroUsize>()
+                .map(|n| usize::from(n) - 1)
+                .map_err(|_| value_error())
+            }
+        )
+    
+}
+
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+
+    let range_re = Regex::new(r"^(\d+)-(\d+)$)").unwrap();
+
+    range.split(",")
+        .into_iter()
+        .map(|val| {
+            parse_index(val).map(|n| n..n+1)
+            .or_else(
+                |e| {
+                    range_re.captures(val).ok_or(e).and_then(|captures| {
+
+                        let n1 = parse_index(&captures[1])?;
+                        let n2 = parse_index(&captures[2])?;
+                        
+                        if n1>=n2 {
+                            return Err(format!("First number in range ({}) \
+                             must be lower than second number ({})", n1+1, n2+1));
+                        }
+
+                        Ok(n1..n2+1)
+                    }
+                )
+                }
+            )})
+            .collect::<Result<_, _>>()
+            .map_err(From::from)
 }
